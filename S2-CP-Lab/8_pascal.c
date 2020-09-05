19a3516e7f956333b30ff167fb9f57fbc270483b
nandan-unni <asnqln@gmail.com>
2020-09-05 23:36:51 +0530

    7-11 done

--- S2-CP-Lab/8_pascal.c
+++ S2-CP-Lab/8_pascal.c
@@ -4,7 +4,27 @@
 
 #include <stdio.h>
 void main()
-{}
+{
+    int rows, i, j, k, no;
+    printf("\n Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (k = 1; k < rows - i; k++)
+            printf("  ");
+        for (j = 0; j <= i; j++)
+        {
+            if (j == 0)
+                no = 1;
+            else
+                no = no * (i - j + 1) / j;
+            printf(" %2d ", no);
+        }
+        printf("\n");
+    }
+
+}
 
 /*
 OUTPUT
--- S2-CP-Lab/9_avg.c
+++ S2-CP-Lab/9_avg.c
@@ -4,7 +4,21 @@
 
 #include <stdio.h>
 void main()
-{}
+{
+    int a[20], no, i, sum = 0;
+    float avg;
+    printf("\n Enter the number of elements in the array: ");
+    scanf("%d", &no);
+    printf("\n Enter the elements: \n");
+    for (i = 0; i < no; i++)
+    {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+    avg = sum / no;
+    printf("\nThe sum of the elements in the given array is %d", sum);
+    printf("\nThe average of the elements in the given array is %f", avg);
+}
 
 /*
 OUTPUT
